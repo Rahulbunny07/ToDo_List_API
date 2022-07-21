7bc4b0891ee99f6d2ffd353710b0585d8a6b23de
Rahul Taritla <78720486+Rahulbunny07@users.noreply.github.com>
2022-07-21 19:29:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+window.onload=()=>{
+  Show_All()
+}
+
+function Show_All(){
+  document.getElementById('head').innerHTML='Todo list - All' //    <h1 id="head"></h1>
+  let todo = document.getElementById('todos')  // <div id="todo"></div>
+  todo.innerHTML = '';
+  fetch(`https://jsonplaceholder.typicode.com/todos`).then(response=>{
+      return response.json()
+  }).then(data=>{
+      console.log(data)
+     for(let each of data){
+         //console.log(each)
+         let p = document.createElement('p')
+         
+         if(each.completed){
+          p.style.color ="green";
+         }else{
+          p.style.color ="red";
+         }
+         p.textContent =each.title;
+         todo.appendChild(p)
+     }
+  }).catch(data=>{
+      alert("Error occured")
+  })
+}
+
+function Completed(){
+  document.getElementById('head').innerHTML='Todo list - Completed'
+  let todo = document.getElementById('todos')
+  todo.innerHTML = '';
+  fetch(`https://jsonplaceholder.typicode.com/todos`).then(response=>{
+      return response.json()
+  }).then(data=>{
+      //console.log(data)
+     for(let each of data){
+         //console.log(each)
+         let p = document.createElement('p')
+         if(each.completed){
+          p.textContent =each.title;
+         todo.appendChild(p)
+         }
+     }
+  })
+}
+
+function Pending(){
+  document.getElementById('head').innerHTML='Todo list - Pending'
+  let todo = document.getElementById('todos')
+  todo.innerHTML = '';
+  fetch(`https://jsonplaceholder.typicode.com/todos`).then(response=>{
+      return response.json()
+  }).then(data=>{
+      //console.log(data)
+     for(let each of data){
+         //console.log(each)
+         let p = document.createElement('p')
+         if(!each.completed){
+          p.textContent =each.title;
+         todo.appendChild(p)
+         }
+     }
+  })
+}
